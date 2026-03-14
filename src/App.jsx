fbff30e150c2cdc3d2aae08740e8895d8a98aa4f
ShehabM0 <shehabmohamed2357@gmail.com>
2026-03-14 16:49:57 +0200

    add social icons container

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
-import { useEffect, useState } from "react";
-import './App.css'
+import LinkedinLogo from "./assets/linkedin.svg"
+import GithubLogo from "./assets/github.svg"
+import MailLogo from "./assets/mail.svg"
+
+import { useEffect, useState } from "react"
+import "./App.css"
 
 const text =
   "Backend engineer who enjoys building reliable systems and turning ideas into real products expressing them through mobile applications and web platforms";
@@ -34,11 +38,24 @@ export default function App() {
       </div>
 
       {/* Intro */}
-      <h1 className="center">Software Development Engineer</h1>
+      <h1 className="title">Software Development Engineer</h1>
       <p className="typewriter">
         {displayText}
         <span className={`cursor ${done ? "cursor-done" : ""}`} />
       </p>
+
+      {/* Icons */}
+      <div className="icons">
+        <a className="link" href="mailto:shehabmohamed2357@gmail.com" target="_blank" >
+          <img className="icon" src={MailLogo} />
+        </a>
+        <a className="link" href="https://github.com/ShehabM0" target="_blank" >
+          <img className="icon" src={GithubLogo} />
+        </a>
+        <a className="link" href="https://www.linkedin.com/in/shehab-mohamed-4284b8249/" target="_blank" >
+          <img className="icon" src={LinkedinLogo} />
+        </a>
+      </div>
 
     </main>
   );
