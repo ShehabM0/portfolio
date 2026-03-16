a311c99f2ba806b719284e07445b8da94b7123dc
ShehabM0 <shehabmohamed2357@gmail.com>
2026-03-16 03:22:05 +0200

    add terminal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import TerminalLogo from "./assets/terminal.svg"
 import LinkedinLogo from "./assets/linkedin.svg"
 import GithubLogo from "./assets/github.svg"
 import MailLogo from "./assets/mail.svg"
@@ -55,6 +56,52 @@ export default function App() {
         <a className="link" href="https://www.linkedin.com/in/shehab-mohamed-4284b8249/" target="_blank" >
           <img className="icon" src={LinkedinLogo} />
         </a>
+      </div>
+
+      {/* About */}
+      <div className="section-title">
+        About me
+      </div>
+
+      {/* Terminal */}
+      <div className="terminal">
+        <div className="terminal-header">
+          <div>
+            <img className="terminal-icon" src={TerminalLogo} />
+          </div>
+          <div>
+            <span className="terminal-title">shehab@portfolio:~</span>
+          </div>
+          <div> </div>
+        </div>
+
+        <div className="terminal-body">
+          <p><span className="prompt">$</span> whoami</p>
+          <p>→ Shehab, a Computer Science graduate from Cairo University in 2024</p>
+
+          <br></br>
+
+          <p><span className="prompt">$</span> cat graduation.txt</p>
+          <p>
+            ✓ Bachelor of Computer Science Cairo University <br/>
+            ✓ Graduation Project: Hospital Management System (Excellent with Honors) <br/>
+            ✓ Graduated with Very Good with Honors
+          </p>
+
+          <br></br>
+
+          <p><span className="prompt">$</span> bash military.sh</p>
+          <p>
+            Military status is <span style={{color: "green"}}>completed</span>
+          </p>
+
+          <br></br>
+          
+          <p>
+            <span className="prompt">$</span> 
+            <span className="terminal-cursor" />
+          </p>
+        </div>
       </div>
 
     </main>
