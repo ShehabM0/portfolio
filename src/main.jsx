4f5e3a411efafb636ba4095f647b5b94c7a924ae
ShehabM0 <shehabmohamed2357@gmail.com>
2026-03-19 17:41:41 +0200

    add font weight

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+import "@fontsource/iosevka/300.css";
+import "@fontsource/iosevka/400.css";
+import "@fontsource/iosevka/500.css";
+import "@fontsource/iosevka/700.css";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
